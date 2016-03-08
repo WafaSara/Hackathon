f57a3eaa61ffbac22eb0ad11c898958ac1a64d5a
Wafae Sara <wafaeben775@gmail.com>
2016-03-08 16:22:54 +0100

    commit initiale

---
+++ database/migrations/2016_03_08_160704_create_reservation_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            //
+        });
+    }
+}
