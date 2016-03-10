b9e57d5795061385bb4acadf05436c2e51cb2479
Wafae Sara <wafaeben775@gmail.com>
2016-03-10 18:01:45 +0100

    modal

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -878,8 +878,10 @@ var g_place_init = "false";
 	<div class="moitie">
 
 		<div style="display:inline-block; position: relative">
-			<div class="fb-video" data-allowfullscreen="1" data-href="{{$video->source}}" data-width="330" data-height="165"><div class="fb-xfbml-parse-ignore"><blockquote cite="{{$video->source}}"><a href="{{$video->source}}"></a></blockquote></div></div>
-			<div data-toggle="modal" data-target="#myModal"
+			<div class="fb-video" data-allowfullscreen="1" data-href="{{$video->source}}" data-width="330" data-height="165">
+				<div class="fb-xfbml-parse-ignore"><blockquote cite="{{$video->source}}"><a href="{{$video->source}}"></a></blockquote></div>
+		</div>
+			<div data-toggle="modal" data-target="#myModal{{$video->id}}"
 				style="z-index:99999; position: absolute; top:0; left:0; bottom:0; right:0; width:110%">
 			</div>
 		</div>
@@ -898,14 +900,14 @@ var g_place_init = "false";
 		</div>
 
 		<!-- Modal -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal fade" id="myModal{{$video->id}}" tabindex="-1" role="dialog" aria-labelledby="">
 			<div class="modal-dialog" role="document">
 			<div class="modal-content">
 
 				<div class="modal-body">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 
-				 <div class="fb-video" data-allowfullscreen="1" data-href="{{$video->source}}" data-width="680"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/lecoupdoeilofficiel/videos/569403649888874/"><a href="https://www.facebook.com/lecoupdoeilofficiel/videos/569403649888874/">Top des choses que vous devez savoir !</a><p>Top des choses que vous devez savoir !</p>Posté par <a href="https://www.facebook.com/lecoupdoeilofficiel/">Le Coup D&#039;Oeil</a> sur mardi 8 mars 2016</blockquote></div></div>
+				 <div class="fb-video" data-allowfullscreen="1" data-href="{{$video->source}}" data-width="680"><div class="fb-xfbml-parse-ignore"><blockquote cite="{{$video->source}}"><a href="{{$video->source}}"></a></blockquote></div></div>
 
 			 </div>
 				</div>
